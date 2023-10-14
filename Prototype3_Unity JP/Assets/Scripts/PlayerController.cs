f299e1fbaad9a1f38c011c12433b208c9de36afa
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-14 20:30:05 +0100

    lesson 3.3 finished
    animations added and some additional functionality

--- Prototype3_Unity JP/Assets/Scripts/PlayerController.cs
+++ Prototype3_Unity JP/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,13 @@ public class PlayerController : MonoBehaviour
 
     public bool isGameOver = false;
 
+    private Animator playerAnim;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
        
     }
@@ -25,6 +28,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)&& isOnGround){
              playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
              isOnGround = false;
+             playerAnim.SetTrigger("Jump_trig");
+        } else if (isGameOver == true){
+            isOnGround = false;
         }
     }
     private void OnCollisionEnter(Collision collision) {
@@ -34,6 +40,9 @@ public class PlayerController : MonoBehaviour
         } else if (collision.gameObject.CompareTag("Obstacle")){
             Debug.Log ("Game Over");
             isGameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            
         }
     }
 }
